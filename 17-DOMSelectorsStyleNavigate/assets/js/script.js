3c4785e993266148c1fe67ecc17b6e065404debd
suleymanlimehri <mehribansuleymanli153@gmail.com>
2025-04-09 01:22:53 +0400

    dom

---
+++ 17-DOMSelectorsStyleNavigate/assets/js/script.js
@@ -0,0 +1,123 @@
+// task1
+// 1. Card uzerinde navigate ederek butun datalari bir 
+// objecte yigaraq consola yazdirin.Productu wishliste elave 
+// edirsiz ona gore heart clasina sahib olan div-den yola cixacaqsiniz.
+// Her iki usulla ilk olaraq elementler uzerinde 
+// kecid ederek elave edeceksiniz daha sonra closest istfade ederek.
+
+
+
+// 1ci usulnan
+// let image=document.querySelector("img").src
+// let title=document.querySelector(".cardTitle").textContent
+// let text=document.querySelector(".cardText").textContent
+// let price=document.querySelector(".cardPrice").textContent
+// let btn=document.querySelector(".shopBtn").textContent
+
+// let obj={
+//     image,
+//     title,
+//     text,
+//     price,
+//     btn
+// }
+
+// console.log(obj)
+
+
+// 2ci usul-closest ile
+// let heart=document.querySelector(".heart i")
+// let image=document.querySelector("img").src
+// let title=document.querySelector(".cardTitle").textContent
+// let text=document.querySelector(".cardText").textContent
+// let price=document.querySelector(".cardPrice").textContent
+// let btn=document.querySelector(".shopBtn")
+
+// let card=heart.closest(".card")
+// let obj={
+//     image,
+//     title,
+//     text,
+//     price,
+//     btn,
+// }
+
+// console.log(obj)
+
+// task2
+// let card=document.querySelector(".card")
+// card.style.margin="50px"
+// card.style.width="300px"
+// card.style.border="1px solid black"
+// card.style.padding="10px"
+
+// let heart=document.querySelector(".heart")
+// heart.style.display="inline-flex"
+// heart.style.alignItems="center"
+// heart.style.justifyContent="center"
+// heart.style.width="30px"
+// heart.style.height="30px"
+// heart.style.borderRadius="50%"
+// heart.style.backgroundColor="silver"
+// heart.style.position="absolute"
+// heart.style.top='15px'
+// heart.style.cursor='pointer'
+
+// let image=document.querySelector(".image")
+// image.style.width="100%"
+// image.style.height='100%'
+// image.style.position="relative"
+
+// let cardcont=document.querySelector(".cardContent")
+// cardcont.style.display="flex"
+// cardcont.style.flexDirection="column"
+// cardcont.style.alignItems="center"
+// cardcont.style.gap="10px"
+// cardcont.style.marginTop="20px"
+// cardcont.style.color="burlywood"
+
+// let img=document.querySelector("img")
+// img.style.width="100%"
+// img.style.height="100%"
+// img.style.borderRadius="10px"
+
+// let price=document.querySelector(".cardPrice")
+// price.style.display="inline-block"
+// price.style.padding='5px'
+// price.style.borderRadius='5px'
+// price.style.backgroundColor='rgb(95,94,91)'
+
+// let btn=document.querySelector(".shopBtn")
+// btn.style.width="100%"
+// btn.style.padding="10px"
+// btn.style.backgroundColor="skyblue"
+// btn.style.border="none"
+// btn.style.cursor="pointer"
+// btn.style.color="white"
+// btn.style.textTransform="uppercase"
+// btn.style.borderRadius="5px"
+// btn.style.fontWeight="bold"
+
+
+
+// task-3:
+// let heart=document.querySelector(".heart")
+// heart.textContent='hearrrttt'
+// console.log(heart)
+// let image=document.querySelector("img")
+// image.textContent="image"
+// console.log(image)
+// let title=document.querySelector(".cardTitle")
+// title.textContent="title"
+// console.log(title)
+// let text=document.querySelector(".cardText")
+// text.textContent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur, in."
+// console.log(text)
+// let price=document.querySelector(".cardPrice")
+// price.textContent='300AZN'
+// console.log(price)
+// let btn=document.querySelector(".shopBtn")
+// btn.textContent='Newww!! Newwww!!'
+// console.log(btn) 
+
+
---
+++ 17-DOMSelectorsStyleNavigate/assets/js/script2.js
@@ -0,0 +1,141 @@
+// let card = document.createElement("div");
+// card.classList.add("card");
+
+// let image = document.createElement("div");
+// image.classList.add("image");
+
+// let icon = document.createElement("div");
+// icon.classList.add("icon");
+
+// let iconsi = document.createElement("i");
+// iconsi.classList.add("fa-regular", "fa-heart");
+
+// let img = document.createElement("img");
+// img.classList.add("img");
+// img.setAttribute("src", "https://picsum.photos/400/250");
+
+// icon.appendChild(iconsi);
+// image.append(icon, img);
+
+// let text = document.createElement("div");
+// text.classList.add("text-content");
+
+// let paraqraf = document.createElement("p");
+// paraqraf.classList.add("house");
+// paraqraf.textContent = "DETACHED HOUSE  5Y OLD";
+
+// let price = document.createElement("span");
+// price.classList.add("price");
+// price.textContent = "$750,000";
+
+// let address = document.createElement("p");
+// address.classList.add("address");
+// address.textContent = "742 Evergreen Terrace";
+
+// let icons = document.createElement("div");
+// icons.classList.add("icons");
+
+// let iconsbed = document.createElement("div");
+// iconsbed.classList.add("iconsbed");
+
+// let iconsbedi = document.createElement("i");
+// iconsbedi.classList.add("fa-solid", "fa-bed");
+
+// let iconsbedstrong = document.createElement("strong");
+// iconsbedstrong.textContent = "3";
+
+// let iconsbedspan = document.createElement("span");
+// iconsbedspan.textContent = "Bedrooms";
+
+
+
+// let iconsbath = document.createElement("div");
+// iconsbath.classList.add("iconsbath");
+
+// let iconsbathi = document.createElement("i");
+// iconsbathi.classList.add("fa-solid", "fa-bath");
+
+// let iconsbathstrong = document.createElement("strong");
+// iconsbathstrong.textContent = "2";
+
+// let iconsbathspan = document.createElement("span");
+// iconsbathspan.textContent = "Bathrooms";
+
+
+// let realtor = document.createElement("div");
+// realtor.classList.add("realtor");
+// let realtorhead=document.createElement("h3")
+// let realtorImgDiv = document.createElement("div");
+// realtorImgDiv.classList.add("realtor-image");
+
+// let realtorImg = document.createElement("img");
+// realtorImg.setAttribute("src", "https://picsum.photos/50/50");
+
+
+// let contact = document.createElement("div");
+// contact.classList.add("contact");
+
+// let contactName = document.createElement("div");
+// contactName.classList.add("contact-header");
+// contactName.textContent = "Tiffany Heffner";
+
+// let contactPhone = document.createElement("div");
+// contactPhone.classList.add("contact-span");
+// contactPhone.textContent = "(555) 555-4321";
+
+// text.append(paraqraf, price, address);
+// contact.append(contactName, contactPhone);
+// iconsbed.append(iconsbedi, iconsbedstrong, iconsbedspan);
+// iconsbath.append(iconsbathi, iconsbathstrong, iconsbathspan);
+// icons.append(iconsbed, iconsbath);
+// realtorImgDiv.appendChild(realtorImg);
+// realtor.append(realtorImgDiv, contact);
+// card.append(image, text, icons, realtor);
+// body=document.querySelector("body")
+// document.body.appendChild(card);
+
+// console.log(body)
+// // stilleriiii
+// card.style.margin="50px"
+// card.style.width="450px"
+// img.style.borderRadius="10px"
+// card.style.height="650px"
+// card.style.border="1px solid grey"
+// card.style.padding="3px"
+// image.style.width="100%"
+// image.style.margin="0"
+// image.style.position="relative"
+// img.style.width="100%"
+// img.style.height="400px"
+// iconsi.style.display="inline-flex"
+// iconsi.style.alignItems="center"
+// iconsi.style.justifyContent="center"
+// iconsi.style.borderRadius="50%"
+// iconsi.style.color="white"
+// iconsi.style.position="absolute"
+// text.style.marginLeft="10px"
+// iconsi.style.top='15px'
+// iconsi.style.right='25px'
+// iconsi.style.cursor='pointer'
+// price.style.fontWeight="400"
+// price.style.fontSize="40px"
+// icons.style.display="flex"
+// icons.style.marginLeft="10px"
+// icons.style.justifyContent="space-between"
+// iconsbedi.style.marginRight="5px"
+// iconsbedi.style.fontSize="30px"
+// iconsbath.style.marginRight="10px"
+// iconsbathi.style.marginRight="5px"
+// iconsbathi.style.fontSize="30px"
+// icons.style.marginBottom="20px"
+// iconsbedstrong.style.marginRight="3px"
+// iconsbathstrong.style.marginRight="5px"
+// iconsbathspan.style.marginRight="3px"
+// realtor.style.display="flex"
+// realtor.style.marginRight="10px"
+// realtor.style.gap="10px"
+// realtorImgDiv.style.width="50px"
+// realtorImgDiv.style.height="50px"
+// realtorImg.style.width="100%"
+// realtorImg.style.height="100%"
+// realtorImg.style.borderRadius="30%"
